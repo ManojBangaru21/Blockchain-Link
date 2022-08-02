7d0599ec2af3645a667b5d3b59da12f5096820f6
Manoj21 <bmanoj.ba@gmail.com>
2022-08-02 11:28:25 -0700

    Prime Number JS

--- HTMLCSSJS/FirstProg.js
+++ HTMLCSSJS/FirstProg.js
@@ -1,11 +1,16 @@
-console.log("Welcome Back Aliens")
+console.log(`Welcome Back Aliens`) //print the output
 // Print all even numbers from 1 to 100
 
-let x =1
-
-if(x%2==0)
-{
-    console.log(x)
+ let x =1 // to declare a variable
+ while(x<=100)
+ {
+     if(x%2===0)
+ {
+ console.log(`${x} is even`)
+ }
+ /*else
+ {
+     console.log(`this is  not necessary`)
+ }*/
+ x++
 }
-else
-x++
