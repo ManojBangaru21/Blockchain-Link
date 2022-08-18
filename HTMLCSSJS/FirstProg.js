f2c412195db299d2cf48e9501cf7438fc8a36488
Manoj21 <bmanoj.ba@gmail.com>
2022-08-18 20:01:58 +0530

    JS Relational operators

--- HTMLCSSJS/FirstProg.js
+++ HTMLCSSJS/FirstProg.js
@@ -1,6 +1,6 @@
-console.log(`Welcome Back Aliens`) //print the output
+//console.log(`Welcome Back Aliens`) //print the output
 // Print all even numbers from 1 to 100
-
+/*
  let x =1 // to declare a variable
  while(x<=100)
  {
@@ -12,5 +12,28 @@ console.log(`Welcome Back Aliens`) //print the output
  {
      console.log(`this is  not necessary`)
  }*/
- x++
-}
+ /*x++
+}*/
+
+/*let num=1
+//num +=2 // num = num + 2
+console.log(num)
+
+//num++ //Post increment
+console.log(num)
+let num1 = ++num //++num is preincrement
+console.log(num,num1)*/
+
+// Relational Operators
+
+let a = 10
+let b = 12
+
+let result = a > b
+// Template literal -  ${}
+console.log(` a is greater than b result`)
+console.log(` ${a} is greater than ${b} is ${result}`)
+
+// Use '===' to compare values and type
+// use '=' for assignment
+// Use '==' for compare values but not type
