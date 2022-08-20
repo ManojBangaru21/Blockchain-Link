282e52defecee27c6d0dfe67c35a827a1bd6f52f
Manoj21 <bmanoj.ba@gmail.com>
2022-08-20 18:39:19 +0530

    Objects

--- HTMLCSSJS/functions.js
+++ HTMLCSSJS/functions.js
@@ -10,3 +10,27 @@
 // }
 
 // console.log(add(8,5))
+
+// Objects are the instances of a class.
+// first method creating an object
+// let obj1=new Object()
+// obj1.name = "Manoj"
+// obj1.age = 29
+// obj1.sex = "Male"
+// obj1.educated  = "yes"
+// obj1.marital_status = "Married"
+
+// console.log(obj1)
+
+// Second method of creating object
+
+// let obj2 = {
+//     Name:"Manoj",
+//     // name = "Manoj"
+// "age" : 29,
+// sex : "Male",
+// educated  : "yes",
+// marital_status : "Married"
+// }
+// console.log(obj2)
+
