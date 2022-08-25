91e91f47f3443c149dcd716f07ba58bae95f04f3
ManojBangaru21 <bmanoj.ba@gmail.com>
2022-08-25 07:17:06 +0530

    Car inheritence

--- HTMLCSSJS/Oops.js
+++ HTMLCSSJS/Oops.js
@@ -1,15 +1,15 @@
 //creation of class 
 
-class Person{
-    constructor(name) // constructor will assign the values. One class can have only one constructor
-   {
-        this.name=name
-   }  
-   greet(){
-        console.log(`Hello ${this.name}`)
+// class Person{
+//     constructor(name) // constructor will assign the values. One class can have only one constructor
+//    {
+//         this.name=name
+//    }  
+//    greet(){
+//         console.log(`Hello ${this.name}`)
 
-   }
-}
+//    }
+// }
 
 // let person1 = new Person("Manoj") // instead of 'let' we can use 'const' also. 'Const' refers to constant value, we cannot reassign a new value
 
@@ -27,19 +27,43 @@ class Person{
 // let human1 = new human(`Manoj Inheritence`)
 // human1.greet()
 
-class student extends Person{
-    constructor(name,age,sex){
-       super(name,age,sex)  // Use of 'Super' keyword enables to point the constructor of parent class
+// class student extends Person{
+//     constructor(name,age,sex){
+//        super(name)  // Use of 'Super' keyword enables to point the constructor of parent class
+//         this.name=name
+//         this.age=age
+//         this.sex=sex
+//     }
+//     greet(){
+//         console.log(this.name)
+//         console.log(this.age)
+//         console.log(this.sex)
+//     }
+// }
+
+// let student1 = new student ('Likitha',26,'F')
+// student1.greet()
+
+
+
+class car{
+    constructor(name,brand){
         this.name=name
-        this.age=age
-        this.sex=sex
+        this.brand=brand
     }
-    greet(){
-        console.log(this.name)
-        console.log(this.age)
-        console.log(this.sex)
-    }
+   
 }
 
-let student1 = new student ('Likitha',26,'F')
-student1.greet()
+class Maruti extends car{
+    constructor(name,make)
+{   super(name)
+    this.name=name
+    this.make=make
+}
+print()
+{
+    console.log(this.name,this.make)
+}
+}
+let Maruti1 = new Maruti("Honda","Kia")
+Maruti1.print()
