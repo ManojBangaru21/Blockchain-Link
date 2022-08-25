3b123c38b626d6013b54274f0d8e05e02ea7070b
ManojBangaru21 <bmanoj.ba@gmail.com>
2022-08-25 19:08:27 +0530

    Arrays

---
+++ HTMLCSSJS/arrays.js
@@ -0,0 +1,18 @@
+// let a = [1,2,3]
+
+// //console.log(a.length)
+// a.push(4) // add an element at the end of an array
+// a.unshift(0) // add an element at the beginning of an array
+// a.pop(2) // removes an item from the end
+// a.shift()// removes first element of an array
+// //console.log(a)
+
+// let b = [10,`Manoj`,[5,6]]
+// //console.log(b)
+
+// let c = a.concat(b) // add two arrays
+
+// //console.log(c)
+
+// let d = [...a,...b] // using spread operator '...' to add 2 arrays
+// console.log(d)
