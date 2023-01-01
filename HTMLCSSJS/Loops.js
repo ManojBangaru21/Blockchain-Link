817a6baa544aa9d0485a9dc1395093e879e9f699
ManojBangaru21 <bmanoj.ba@gmail.com>
2023-01-01 18:34:50 +0530

    Restart

--- HTMLCSSJS/Loops.js
+++ HTMLCSSJS/Loops.js
@@ -1,20 +1,20 @@
 // for loop
 // Code for Prime numbers using nested for loop
 
-/*for(let num1 = 2; num1 <= 100; num1++)
-{
-    let prime = 1
-    for(let j=2;j < num1;j++)
-    {
+// for(let num1 = 1; num1 <= 10; num1++)
+// {
+//     let prime = 1
+//     for(let j=2;j < num1;j++)
+//     {
         
-        if (num1%j===0 )
-        {
-           prime++
-        }             
-    }
-   if(prime===1)
-   console.log(`${num1} is prime`)  
-} */
+//         if (num1%j===0 )
+//         {
+//            prime++
+//         }             
+//     }
+//    if(prime===1)
+//    console.log(`${num1} is a Prime Number`)  
+// }
 
 //While loop (Pre-increment condition)
 
@@ -25,11 +25,11 @@
 // }
 
 //do-while loop (Post-increment loop)
-let count = 1
-do{
-    console.log("Hello world")
-    count++
-}while(count<5)
+// let count = 1
+// do{
+//     console.log("Hello world")
+//     count++
+// }while(count<5)
 
 
 // Switch case 
